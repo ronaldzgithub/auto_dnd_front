8c29287a12333fe62bb1942de6ad65293f140449
ronaldzgithub <36839548+ronaldzgithub@users.noreply.github.com>
2024-03-19 09:17:07 +0800

    link to localhost

--- serverComms.js
+++ serverComms.js
@@ -3,8 +3,8 @@
 // NEW - generic send with callback
 
 export async function sendMessage(endpoint, message) {
-    //const base_url = 'http://127.0.0.1:8000' // local
-    const base_url = 'https://auto-dnd-e6db93336cd9.herokuapp.com'
+    const base_url = 'http://127.0.0.1:8000' // local
+    // const base_url = 'https://auto-dnd-e6db93336cd9.herokuapp.com'
     console.log(`sending message ${message}, to ${base_url}/${endpoint}`)
     const resp = await fetch(`${base_url}/${endpoint}`, {
         method: 'POST',
